91fb3db0bcd47bee73cdba0f080ed6b54d3f61bb
alexHooper <alexmohack@gmail.com>
2018-09-03 11:36:42 +0530

    move the code into __main__ file

--- integrate/utils/templates.py
+++ integrate/utils/templates.py
@@ -14,18 +14,3 @@ def get_template(path):
 
 def render_context(template_string, context):
 	return template_string.format(**context)
-
-
-template = get_template(r'templates\email_message.txt')
-template_html = get_template(r'templates\email_message.html')
-
-context = {
-	'name': 'Pranav',
-	'date': '15th Aug, 18',
-	'total': 599
-}
-
-print(render_context(template, context))
-print(render_context(template_html, context))
-
-render_html = render_context(template_html, context)
